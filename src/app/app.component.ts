9c451e58ac6a90cc705e36e1fefa2215893daa95
vils007 <vils007@mail.ru>
2017-12-13 23:05:48 +0300

    add *ngFor в цикле вывожу карточки новостей. + добавил поле news.title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app - news';
+  news = [
+    {title: 'title1'},
+    {title: 'title2'},
+    {title: 'title3'},
+    {title: 'title4'},
+    {title: 'title5'},
+    {title: 'title6'},
+    {title: 'ПО русски'},
+  ];
 }
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,8 +1,13 @@
-import { Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-news',
-  templateUrl: './news.component.html'
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.scss']
 
 })
-export class NewsComponent {}
+export class NewsComponent {
+
+  @Input() news;
+
+}
